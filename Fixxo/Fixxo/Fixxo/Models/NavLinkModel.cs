ec9f1c6698b7496df66d532d148d2d6be7ca6507
Mahmoud <112482449+Mahmoud9222@users.noreply.github.com>
2023-03-13 13:05:15 +0100

    Add files via upload

---
+++ Fixxo/Fixxo/Fixxo/Models/NavLinkModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Fixxo.Models
+{
+    public class NavLinkModel
+    {
+        public string controllerName { get; set; } = null!;
+        public string ActionName { get; set; } = null!;
+        public string Name { get; set; } = null!;
+    }
+}
